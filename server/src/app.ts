16144ff287fcf115f311b87271d75dac78add3dc
Simão Junior <simao.msjr@gmail.com>
2021-05-21 22:05:44 -0300

    feat(): add koa playground

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,18 @@
-import Koa from 'koa';
+import Koa, { Request, Response } from 'koa';
 import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
+import convert from 'koa-convert';
+import cors from 'koa-cors';
+
+import { koaPlayground } from 'graphql-playground-middleware';
 import graphqlHTTP from 'koa-graphql';
 import { buildSchema } from 'graphql';
 
 const app = new Koa();
 const router = new Router();
+
+app.use(bodyParser());
+app.use(convert(cors({ maxAge: 86400, credentials: true })));
 
 const schema = buildSchema(`
 	type Query {
@@ -12,21 +20,43 @@ const schema = buildSchema(`
 	}
 
 `);
-
 const root = {
 	hello: () => 'Hello challenge!',
 };
 
-app.on('error', error => console.log('Server error', error));
+router.get('/', ctx => {
+	const info = ['/graphql -- GraphiQL', '/playground -- GraphQL Playground'];
 
-router.all(
-	'/graphql',
-	graphqlHTTP({
-		schema,
-		graphiql: true,
-		rootValue: root,
+	ctx.status = 200;
+	ctx.body = info.join('/n');
+});
+
+router.all('/playground', koaPlayground({ endpoint: '/graphql' }));
+
+const appGraphQL = convert(
+	graphqlHTTP(async (request: Request, ctx: Response, koaContext) => {
+		return {
+			graphiql: false,
+			schema,
+			rootValue: {
+				request: ctx.req,
+			},
+			context: {
+				koaContext,
+			},
+		};
 	}),
 );
+
+// router.all(
+// 	'/graphql',
+// 	graphqlHTTP({
+// 		schema,
+// 		graphiql: true,
+// 		rootValue: root,
+// 	}),
+// );
+router.all('/graphql', appGraphQL);
 
 app.use(router.routes()).use(router.allowedMethods());
 
